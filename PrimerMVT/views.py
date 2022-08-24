81815c270219fb3a9af5d2349354e5dc43e8f160
Patricio Mlekuz <patomlekuz2@gmail.com>
2022-08-23 22:29:50 -0300

    PrimerMVTENTREGABLE

--- PrimerMVT/views.py
+++ PrimerMVT/views.py
@@ -13,13 +13,16 @@ def familia(request):
     familia_1.save
     familia_2=Familia(nombre="Micaela", apellido="Cubito",edad=35)
     familia_2.save
-    diccionario={"nombre":familia.nombre,"apellido":familia.apellido,"edad":familia.edad,
-    "nombre_1":familia_1.nombre,"apellido_1":familia_1.apellido,"edad_1":familia_1.edad,
-    "nombre_2":familia_2.nombre,"apellido_2":familia_2.apellido,"edad_2":familia_2.edad}
+    
+    
+    
     lista_nombres=[familia.nombre,familia_1.nombre,familia_2.nombre]
     lista_apellidos=[familia.apellido,familia_1.apellido,familia_2.apellido]
     lista_edades=[familia.edad,familia_1.edad,familia_2.edad]
-    
+    diccionario={"nombre":familia.nombre,"apellido":familia.apellido,"edad":familia.edad,
+    "nombre_1":familia_1.nombre,"apellido_1":familia_1.apellido,"edad_1":familia_1.edad,
+    "nombre_2":familia_2.nombre,"apellido_2":familia_2.apellido,"edad_2":familia_2.edad,
+    "listanombres":lista_nombres,"listaapellidos":lista_apellidos,"listaedades":lista_edades}
     
     plantilla=loader.get_template("template1.html")
     documento= plantilla.render(diccionario)
